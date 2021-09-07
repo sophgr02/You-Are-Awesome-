02a61f526f4890500ee4ec12ed04bd807a8981c8
Sophia Riva <sriva@zagmail.gonzaga.edu>
2021-09-06 19:01:48 -0700

    After 2.6 video. "Show message" button showed a message and a photo

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -9,7 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
-    
+
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
        
@@ -18,6 +19,7 @@ class ViewController: UIViewController {
     @IBAction func messageButtonPressed(_ sender: UIButton) {
        
         messageLabel.text = "You Are Awesome!"
+        imageView.image = UIImage(named: "image0")
     }
     
 }
