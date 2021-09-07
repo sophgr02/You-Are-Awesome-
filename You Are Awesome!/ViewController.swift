c93d68a2164aeea84d27324cd9a1888c9f183f52
Sophia Riva <sriva@zagmail.gonzaga.edu>
2021-09-07 13:15:09 -0700

    video 2.8 - naming messages and defining variables (awesomeMessage, etc.), conditional statements

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -9,18 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
-
     @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        messageLabel.text = ""
+         // don't know if i need this section  -- messageLabel.text = ""
        
     }
 
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-       
-        messageLabel.text = "You Are Awesome!"
-        imageView.image = UIImage(named: "image0")
+       let awesomeMessage = "You Are Awesome!"
+        let greatMessage = "You Are Great!"
+        let bombMessage = "You Are Da Bomb!"
+        
+        
+        if messageLabel.text == awesomeMessage {
+            messageLabel.text = greatMessage
+            imageView.image = UIImage(named: "image1")
+        } else if  messageLabel.text == greatMessage {
+            messageLabel.text = bombMessage
+            imageView.image = UIImage(named: "image2")
+        } else {
+            messageLabel.text = awesomeMessage
+            imageView.image = UIImage(named: "image0")
+        }
     }
     
 }
